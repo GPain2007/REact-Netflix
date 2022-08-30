75dec491f92246b99db86d8ebbea3c506d336164
Govnor Payne <govnorpayne@gmail.com>
2022-08-30 15:32:58 -0500

    made button CSS and componenet libriay

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -2,7 +2,6 @@ import React, { useState, useContext, createContext } from "react";
 import {
   Container,
   Inner,
-  Frame,
   Title,
   Item,
   Header,
@@ -21,10 +20,6 @@ export default function Accordion({ children, ...restProps }) {
 
 Accordion.Title = function AccordionTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
-};
-
-Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
-  return <Frame {...restProps}>{children}</Frame>;
 };
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
