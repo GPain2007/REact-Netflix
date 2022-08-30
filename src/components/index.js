04a25e81afaaaeb7199600d152faf921a7dd6462
Govnor Payne <govnorpayne@gmail.com>
2022-08-30 15:37:12 -0500

    exported OptForm

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Jumbotron } from "./jumbtron";
 export { default as Accordion } from "./accordion";
 export { default as Footer } from "./Footer";
+export { default as OptForm } from "./opt-form";
