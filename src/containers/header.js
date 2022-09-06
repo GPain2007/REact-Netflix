a4cec5aac739f0d133b3c15c4e3bfb54e9380785
Govnor Payne <govnorpayne@gmail.com>
2022-09-06 09:11:55 -0500

    added logo

--- src/containers/header.js
+++ src/containers/header.js
@@ -6,7 +6,7 @@ export function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES.HOME} alt="Netflix" />
+        <Header.Logo to={ROUTES.HOME} alt="Netflix" src="" />
         <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
       </Header.Frame>
       {children}
